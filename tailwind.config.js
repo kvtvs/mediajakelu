47a871d04989b1d227ef9f40e96d9ad8c4c77cfa
kvtvs <kata.sara-aho@metropolia.fi>
2022-10-15 19:41:24 +0300

    :)

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      xs: "350px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      colors: {
+        lightgreen: "rgba(184, 216, 216, 1)",
+        phlox: "rgba(209, 82, 243, 1)",
+        gunmetal: "rgba(29, 38, 53, 1)",
+        "lapis-lazuli": "rgba(30, 88, 132, 1)",
+      },
+      fontFamily: {
+        montrserrat: ['"Montserrat"', "sans serif"],
+      },
+    },
+  },
+  plugins: [],
+};
